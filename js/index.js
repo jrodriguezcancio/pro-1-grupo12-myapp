ece891dd1cc4255f5d0019416e9a74a8d54e8ccb
joacoceliento <jceliento@udesa.edu.com>
2024-11-15 12:50:08 -0300

    llamando la API

--- js/index.js
+++ js/index.js
@@ -1,3 +1,33 @@
-let prueba = alert(holaa)
+let recetas = document.querySelector(".fotorecetas");
+const URL = "https://dummyjson.com/recipes";
+let recetasplus = " ";
 
-let prueba2 = alert(HOLAAAAAA)
+fetch(URL)
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    console.log(data);
+    
+    let lista = data.recipes
+    console.log(lista);
+    
+    for (let i = 0; i < lista.length; i++) {
+        let name = (lista[i].name);
+        let image = (lista[i].image);
+        let id = (lista[i].id);
+        
+        
+        recetasplus += `
+            <article>
+                <p>Name: ${name} </p>
+                <img src="${image}" alt="${name}" class="imagen">
+            </article>
+        `
+    }
+   
+    recetas.innerHTML = recetasplus;
+})
+.catch(function(error) {
+    console.log("Error: " + error);
+});
