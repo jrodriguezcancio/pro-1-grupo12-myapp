14d89b8581f21b751008bf81ae155db84f1112b2
vdominguezz <dominguezv@udesa.edu.com>
2024-11-16 18:58:52 -0300

    Index.html y Index.js

--- js/index.js
+++ js/index.js
@@ -1,33 +1,103 @@
+
 let recetas = document.querySelector(".fotorecetas");
 const URL = "https://dummyjson.com/recipes";
 let recetasplus = " ";
+
 
 fetch(URL)
 .then(function(response) {
     return response.json();
 })
+
 .then(function(data) {
-    console.log(data);
-    
+
     let lista = data.recipes
     console.log(lista);
     
     for (let i = 0; i < lista.length; i++) {
-        let name = (lista[i].name);
-        let image = (lista[i].image);
-        let id = (lista[i].id);
+        let nombre = (lista[i].name);
+        let imagen = (lista[i].image);
+        let dificultad = (lista[i].difficulty)
+        let ingredientes = (lista[i].ingredients)
+        let instrucciones = (lista[i].instructions)
         
         
         recetasplus += `
-            <article>
-                <p>Name: ${name} </p>
-                <img src="${image}" alt="${name}" class="imagen">
+            <article class="article" >
+                <p class= "titulo ">${nombre} </p>
+                <img src="${imagen}" alt="${nombre}" class="imagen">
+                <p class= "nivel" >${dificultad} </p>
+                <a href="./receta.html" class="masINFO" ><p>Más información de la receta</p></a>
             </article>
-        `
-    }
+        `}
    
     recetas.innerHTML = recetasplus;
+
+    let articulos = document.querySelectorAll(".article")
+    let imagenes = document.querySelectorAll(".imagen");
+    let textos = document.querySelectorAll ("p")
+    let titulos = document.querySelectorAll(".titulo")
+    let niveles = document.querySelectorAll(".nivel")
+    let infos = document.querySelectorAll(".masINFO")
+
+    for (let n = 0; n < titulos.length; n++) {
+        const titulo = titulos[n];
+        titulo.style.color = "red"
+        titulo.style.fontSize = "20px"
+        titulo.style.margin = "20px 0px 5px 0px"
+
+    }
+
+    /*for (let t = 0; t < niveles.length; t++) {
+        const nivel = niveles[t];
+        
+    }
+
+    for (let u = 0; u < infos.length; u++) {
+        const info = infos[iiii];
+        
+    }*/
+
+    for (let iiii = 0; iiii < textos.length; iiii++) {
+        const texto = textos[iiii];
+        
+        texto.style.color = "black"
+    }
+
+    for (let ii = 0; ii < imagenes.length; ii++) {
+        const imagen = imagenes[ii];
+        imagen.style.border = 
+        imagen.style.width = "250px"
+        imagen.style.height = "250px"
+        imagen.style.margin = "20px"
+        imagen.style.borderRadius = "20px"
+        imagen.style.border = "1px solid black"
+        imagen.style.filter ="drop-shadow(2px 2px 4px rgb(0, 0, 0))"
+    }
+
+    for (let iii = 0; iii < articulos.length; iii++) {
+        const articulo = articulos[iii];
+        articulo.style.border = "2px solid black";
+        articulo.style.margin = ""
+        articulo.style.display = "flex"
+        articulo.style.flexDirection = "column"
+        articulo.style.flexWrap = "nowrap"
+        articulo.style.alignItems = "center"
+        articulo.style.margin = "30px 15px 10px 15px"
+        articulo.style.borderRadius = "20px"
+        articulo.style.backgroundColor = "#fbfbed"
+    }
+
 })
+
 .catch(function(error) {
     console.log("Error: " + error);
 });
+
+
+recetas.style.display = "flex"
+recetas.style.flexWrap = "wrap"
+recetas.style.alignContent = "center"
+recetas.style.justifyContent = "center"
+recetas.style.margin = "0px"
+
