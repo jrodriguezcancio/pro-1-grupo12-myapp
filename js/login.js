07c767dbff68af72d5ed29e615c71db059ecf050
jrodriguezcancio <jrodriguezcancio@udesa.edu.ar>
2024-11-19 10:50:46 -0300

    login

---
+++ js/login.js
@@ -0,0 +1,19 @@
+let login = document.querySelector(".login")
+
+login.addEventListener('submit', function(e) {
+    let email = (email.value)
+
+if(email == "") {
+    alert("El campo no puede estar vacio");
+    e.preventDefault();
+}
+})
+
+login.addEventListener('submit', function(e) {
+    let contrasennia = (contrasennia.value)
+
+if(contrasennia == "") {
+    alert("El campo no puede estar vacio");
+    e.preventDefault();
+}
+})
