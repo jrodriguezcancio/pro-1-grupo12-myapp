eb06df0f6f6ca1803e70ed0b0d9b1e138750f3ce
joacoceliento <jceliento@udesa.edu.com>
2024-11-27 10:11:01 -0300

    cambie la condicion del buscador para que quede mejor

--- js/search-results.js
+++ js/search-results.js
@@ -7,65 +7,65 @@
     // 2- Para el caso de no haber resultados que coincidan con 
         //el término buscado la página debe avisar al usuario que no hay coincidencias.
 
-
-//Extraigo los valores buscados por el usuario
-let qs = location.search 
+let qs = location.search;
 console.log(qs);
 
 let qsObj = new URLSearchParams(qs);
-let buscado = qsObj.get("buscadorNAME");
+let buscado = qsObj.get("buscadorNAME");  
 console.log(buscado);
 
-
-let URL = `https://dummyjson.com/recipes/search${qs}`;
+let URL = `https://dummyjson.com/recipes/search?q=${buscado}`;
 let recetas = document.querySelector(".fotorecetas");
 let resultados = " ";
 
-//Reutilice codigo y solo agregue el if para el buscador
-fetch(URL)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        let listaRECETAS = data.recipes
+if (buscado) {
+    if (buscado.length >= 3) {
+        fetch(URL)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            let listaRECETAS = data.recipes;
 
-        for (let i = 0; i < listaRECETAS.length; i++) {
-            let nombre = (listaRECETAS[i].name);
-            let imagen = (listaRECETAS[i].image);
-            let id = (listaRECETAS[i].id)
-
-            if (buscado && nombre.toLowerCase().includes(buscado.toLowerCase())) {
-
-                // nombre.toLowerCase() permite que el nombre de la receta se vea en minúscula 
+            for (let i = 0; i < listaRECETAS.length; i++) {
+                let nombre = listaRECETAS[i].name;
+                let imagen = listaRECETAS[i].image;
+                let id = listaRECETAS[i].id;
 
                 resultados += `
-                <article class="article" >
-                    <p class= "titulo ">${nombre} </p>
+                <article class="article">
+                    <p class="titulo">${nombre}</p>
                     <img src="${imagen}" alt="${nombre}" class="imagen">
-                    <a href="./receta.html?/id=${id}" class="masINFO" ><p>More information about the recipe</p></a>
+                    <a href="./receta.html?/id=${id}" class="masINFO"><p>More information about the recipe</p></a>
                 </article>
-                `
+                `;
             }
+
             
-       }        
-        recetas.innerHTML = resultados;
-        let articles = document.querySelectorAll(".article");
+            recetas.innerHTML = resultados;
 
-            // Asignamos eventos de mouseover y mouseout con un ciclo tradicional
-        for (let i = 0; i < articles.length; i++) {
-            let article = articles[i];
+            // Asignamos eventos de mouseover y mouseout a los artículos
+            let articles = document.querySelectorAll(".article");
+            for (let i = 0; i < articles.length; i++) {
+                let article = articles[i];
 
-            article.addEventListener('mouseover', function () {
-                article.style.backgroundColor = "rgb(200, 245, 230)";
-            });
+                article.addEventListener('mouseover', function () {
+                    article.style.backgroundColor = "rgb(200, 245, 230)";
+                });
 
-            article.addEventListener('mouseout', function () {
-                article.style.backgroundColor = ""; // vuelve al color original
-            });
-        }
-    })
-
-    .catch(function(error) {
-        console.log("Error: " + error);
-    });
+                article.addEventListener('mouseout', function () {
+                    article.style.backgroundColor = "";  // Vuelve al color original
+                });
+            }
+        })
+        .catch(function(error) {
+            console.log("Error: " + error);
+        });
+    } else {
+        recetas.innerHTML = "<p>Por favor, busque algo con más de 3 caracteres.</p>";
+    }
+} else {
+    
+    recetas.innerHTML = "<p>Por favor, ingrese un término de búsqueda.</p>";
+}
