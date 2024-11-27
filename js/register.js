7df7bc60e48a782e9b713f2eb4e7bb416846f4be
jrodriguezcancio <jrodriguezcancio@udesa.edu.ar>
2024-11-27 18:24:56 -0300

    js de register

--- js/register.js
+++ js/register.js
@@ -1,2 +1,36 @@
-// (SIN TERMINAR) - Preguntar que es lo que hay que hacer acá 
-let registro = document.querySelector(".registro")
+let registro = document.querySelector(".registro");
+
+let error1= document.querySelector(".errorM");
+let error2= document.querySelector(".errorC");
+
+registro.addEventListener("submit",function(e) {
+
+
+    let isvalid = true;
+
+
+    let email = (emailID.value)
+
+    if(email === ""){
+        error1.style.display= "block"
+        isvalid=false;
+    }
+    else{
+        error1.style.display="none";
+    }
+
+    let password = (contraseñaID.value)
+
+    if(password === ""){
+        error2.style.display= "block"
+        isvalid=false;
+    }
+    else{
+        error2.style.display="none";
+    }
+    if (!isvalid) {
+        e.preventDefault();
+    }
+});
+    
+    
